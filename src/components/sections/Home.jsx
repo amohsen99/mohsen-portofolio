b8857e47fb172bc0b0080f5a923bc99142ca7f29
mohsen <amohsenmohamed571@gmail.com>
2025-06-22 02:29:47 +0300

    [Add]: add revealOnScreen page

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,4 +1,4 @@
-// import { RevealOnScroll } from "../RevealOnScroll";
+import { RevealOnScroll } from "../RevealOnScroll";
 
 export const Home = () => {
   return (
@@ -6,7 +6,7 @@ export const Home = () => {
       id="home"
       className="min-h-screen flex items-center justify-center relative"
     >
-      {/* <RevealOnScroll> */}
+      <RevealOnScroll>
         <div className="text-center z-10 px-4">
           <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-right">
             Hi, I'm Pedro Tech
@@ -34,7 +34,7 @@ export const Home = () => {
             </a>
           </div>
         </div>
-      {/* </RevealOnScroll> */}
+      </RevealOnScroll>
     </section>
   );
 };
